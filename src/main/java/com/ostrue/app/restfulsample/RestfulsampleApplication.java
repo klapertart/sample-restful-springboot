53660871d5bbac523c44c579a7ddd0192650030c
Klapertart <klapertartinc@gmail.com>
2019-10-22 12:58:58 +0700

    new

---
+++ src/main/java/com/ostrue/app/restfulsample/RestfulsampleApplication.java
@@ -0,0 +1,13 @@
+package com.ostrue.app.restfulsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulsampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulsampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ostrue/app/restfulsample/RestfulsampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ostrue.app.restfulsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulsampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
